e2398808290943bf799155b290c2f8ceac0815a6
Shahriat Hossain <somel90@gmail.com>
2021-01-25 09:55:32 +0600

    Spring MVC Hibernate Database Web App

---
+++ web-customer-tracker/src/com/mycode/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.mycode.springdemo.dao;
+
+import java.util.List;
+
+import com.mycode.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+}
